1db1a2cf367d759710bf7f0e39ca393c133f9044
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-11-13 16:34:57 -0500

    Topic Routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,7 @@ import userRouter from './user/index.route.js'
 import courseRouter from './course/index.route.js'
 import authRouter from './auth/index.route.js'
 import classRouter from './class/index.route.js'
+import topicRouter from './topic/index.route.js'
 
 const rootRouter = Router()
 
@@ -10,4 +11,5 @@ rootRouter.use('/auth', authRouter)
 rootRouter.use('/users', userRouter)
 rootRouter.use('/courses', courseRouter)
 rootRouter.use('/classes', classRouter)
+rootRouter.use('/topics', topicRouter)
 export default rootRouter
---
+++ src/routes/topic/index.route.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { topicController } from '../../controllers/index.controllers.js'
+
+const topicRouter = Router()
+
+topicRouter.post('/', topicController.registerTopic)
+topicRouter.get('/', topicController.getAllTopics)
+topicRouter.get('/class/:class_id', topicController.getByClass)
+topicRouter.put('/:id', topicController.updateTopic)
+topicRouter.delete('/:id', topicController.deleteTopic)
+
+export default topicRouter
