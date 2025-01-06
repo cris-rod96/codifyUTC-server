fcd0af03efb020cb0a7c4aca249cdc4ccf76c44e
Cristhian Rodríguez <crisrodam1996@gmail.com>
2025-01-05 20:02:58 -0500

    Database config v2

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -3,7 +3,7 @@ import { DB_DEV, DB_PROD, NODE_ENV } from '../consts/index.consts.js'
 export const CONNECTION = {
   URI: NODE_ENV === 'development' ? DB_DEV : DB_PROD,
   CONFIG:
-    NODE_ENV === 'development'
+    NODE_ENV !== 'development'
       ? {
           logging: false,
           native: false,
